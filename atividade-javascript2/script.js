64452d29c758a312404250a97a120cac7e9ffa1e
EduardoFirminoUcsal <144235776+EduardoFirminoUcsal@users.noreply.github.com>
2023-09-06 09:26:15 -0300

    Add files via upload

---
+++ atividade-javascript2/script.js
@@ -0,0 +1,14 @@
+
+function verificarNumero() {
+  var numero = parseInt(prompt("Digite um número inteiro:"));
+
+  if (numero > 0) {
+    alert("O número é positivo.");
+  } else if (numero < 0) {
+    alert("O número é negativo.");
+  } else {
+    alert("O número é zero.");
+  }
+}
+
+verificarNumero();
